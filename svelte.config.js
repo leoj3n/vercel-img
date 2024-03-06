f9cbe9d2db4cd0d3da49ab7ac6ba063d36bada8c
Joel Kuzmarski <leoj3n@gmail.com>
2024-03-06 06:08:19 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-vercel'
 import { readFileSync } from 'node:fs'
 
 const { version: name } = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'))
-const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
